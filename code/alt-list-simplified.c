a6185c9ed154a6301a9ba16a9399b71114639ae8
Nishant Totla <nishanttotla@gmail.com>
2016-08-26 13:40:40 -0700

    Properly defining program for illustrated interaction example
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- code/alt-list-simplified.c
+++ code/alt-list-simplified.c
@@ -1,16 +1,23 @@
-void alt_list() {
-  bool d = TRUE;
-  List l = cons(d, NIL);
+void alt_list_simplified() {
+  // set of predicates: {p}
+  // allocate an initial node with predicate value TRUE
+  var x = alloc();
+  bool p_val = TRUE
+  set(x, p) = p_val
+  // set y to be the tail of the list
+  var y = x;
+  y->next = NULL
   // LOOP-CONS: build a list with alternating Boolean values.
   while (non_det()) {
-    d = !d;
-    l = cons(d, l);
+    // allocate a new node with alternating value
+    var z = alloc()
+    p_val = !p_val
+    set(z, p) = p_val
+    // append it to the existing list
+    y->next = z
+    y = z
   }
-  // LOOP-CHK: check that the Boolean values alternate.
-  while (l != NIL) {
-    assert(l->data == d);
-    d = !d;
-    l = l->next;
-  }
+  checkpoint:
+  // rest of the program
   return;
 }
